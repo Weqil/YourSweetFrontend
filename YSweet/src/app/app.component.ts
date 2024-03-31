10ac82d78f47d2beb451a0de5e7961add74dc45b
MoskowPsix <bolshe.kivi@gmail.com>
2024-03-31 14:53:28 +0500

    add connect and input, output message websocket

--- YSweet/src/app/app.component.ts
+++ YSweet/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WebsocketService } from './services/websocket.service';
+import { EMPTY, catchError, of } from 'rxjs';
+import { TimeInterval } from 'rxjs/internal/operators/timeInterval';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit{
+  constructor(
+    private websocketService: WebsocketService
+  ) {}
+
+  ngOnInit() {
+    this.websocketService.subject.subscribe(value => {
+      console.log(value)
+      // value.next({m:"m"})
+      // this.websocketService.messages.next({message:"m"})
+    })
+    // this.websocketService.messages.subscribe(value => {
+    //   console.log(value)
+    //   // value.next({m:"m"})
+    //   // this.websocketService.messages.next({message:"m"})
+    // })
+    setTimeout(() => {
+      this.websocketService.sendMessage({messages: 'test'})
+      console.log('send')
+    }, 3000)
+    // this.websocketService.sendMessage({messages: 'test'})
+  }
 }
