c733d4634595a2c6f4e25c035e6f9abd9aacb793
Weqil <114972938+Weqil@users.noreply.github.com>
2024-04-22 11:14:20 +0500

    category delet

--- YSweet/src/app/services/categories.service.ts
+++ YSweet/src/app/services/categories.service.ts
@@ -19,4 +19,13 @@ export class CategoriesService {
   createCategories(category){
    return this.http.post<any>(`${environment.BACK_URL}:${environment.BACK_PORT}/categories`,category)
   }
+  changeCategories(category, id){
+    return this.http.post<any>(`${environment.BACK_URL}:${environment.BACK_PORT}/categories/rename?id=${id}`,category)
+   }
+   deleteCategories(id) {
+    const url = `${environment.BACK_URL}:${environment.BACK_PORT}/categories?id=${id}`;
+    console.log("URL:", url); // Проверяем URL перед отправкой запроса
+    return this.http.delete(url);
+}
+
 }
